d50020cac8d3077e641e4e78435cfa863c6feab8
Brancen Gregory <brancengregory@gmail.com>
2024-08-05 17:08:44 -0500

    Uses gh api to create new repo from template; Inits renv with pak installed

--- R/project.R
+++ R/project.R
@@ -1,14 +1,14 @@
 #' @title Create Project
 #' @description Creates a new R project with a standard directory structure
 #'
-ojo_create_project <- function(name = NULL, dir = ".", private = TRUE) {
+ojo_create_project <- function(name = NULL, description = NULL, dir = ".", private = TRUE, packages = NULL) {
   # Get the initial working directory
   init_wd <- fs::path_wd()
 
   # TODO: Default to using OJO_PATH env var for project location
 
   # Get full path of project directory
-  project_dir <- fs::path_abs(dir, name)
+  project_dir <- fs::path_abs(paste0(dir, "/", name))
 
   # If name is NULL, first try to use interactive CLI, otherwise fail
   if (is.null(name)) {
@@ -22,66 +22,62 @@ ojo_create_project <- function(name = NULL, dir = ".", private = TRUE) {
   # TODO: Check name matches conventions and isn't taken
   # TODO: If interactive, offer to change
 
-  # Init a project
-  usethis::create_project(
-    path = project_dir,
+  # TODO: Check description format
+  # TODO: If description NULL offer to create
+
+  # TODO: Wrap API call and issue custom error message
+  # Create Github repo from project template
+  gh_resp <- gh::gh(
+    "POST /repos/openjusticeok/ojo-project-template/generate",
+    owner = "openjusticeok",
+    name = name,
+    description = description,
+    private = private
+  )
+
+  # Have to give github the time to transfer the repo
+  # TODO: Base this on an API call?
+  Sys.sleep(1)
+
+  usethis::create_from_github(
+    repo_spec = glue::glue("git@github.com:openjusticeok/{name}.git"),
+    destdir = dir,
+    fork = FALSE,
     rstudio = TRUE,
-    open = FALSE
-  )
-
-  # Change working directory to new project
-  setwd(project_dir)
-  usethis::proj_set(project_dir)
-
-  # Add a license
-  usethis::use_gpl3_license()
-
-  # Add a README
-  usethis::use_readme_md(open = FALSE)
-
-  # Prefill readme with title line
-  readr::write_lines(
-    x = paste0("# ", name),
-    file = fs::path(project_dir, "README.md")
-  )
-
-  # TODO: Ask in cli if you want to edit the readme
-
-  # Create a git repo
-  gert::git_init(path = project_dir)
-
-  fs::file_copy(
-    path = system.file(
-      "templates",
-      "gitignore",
-      package = "ojoutils",
-      mustWork = TRUE
-    ),
-    new_path = fs::path(project_dir, ".gitignore"),
-    overwrite = TRUE
-  )
-
-  # Create Github repo
-  usethis::use_github(
-    organisation = "openjusticeok",
-    private = private,
+    open = FALSE,
     protocol = "ssh"
   )
 
-  # Create default directory structure
-  ## .gitkeep files make sure the empty R directory is pushed to git
-  fs::file_create(project_dir, "R", ".gitkeep")
+  # Prefill readme with title line
+  readr::write_lines(
+    x = c(
+      paste0("# ", name),
+      description
+    ),
+    sep = "\n\n",
+    file = fs::path(project_dir, "README.md"),
+    append = FALSE
+  )
 
-  fs::dir_create(project_dir, "data", "input")
-  fs::dir_create(project_dir, "data", "ouput")
-  fs::file_create(project_dir, "data", "input", ".gitkeep")
-  fs::file_create(project_dir, "data", "output", ".gitkeep")
+  # TODO: Ask in cli if you want to edit the readme further
 
-  fs::dir_create(project_dir, "inst", "figures")
-  fs::file_create(project_dir, "inst", "figures", ".gitkeep")
+  # Init renv
+  renv::init(
+    project = project_dir,
+    bare = TRUE,
+    load = FALSE,
+    restart = FALSE
+  )
 
-  fs::dir_create(project_dir, "inst", "reports")
-  fs::file_create(project_dir, "inst", "reports", ".gitkeep")
+  # Add pak
+  renv::install(
+    packages = c(
+      "pak"
+    ),
+    prompt = FALSE,
+    lock = TRUE,
+    project = project_dir
+  )
 
   # Stage all changed files
   gert::git_add(
@@ -91,16 +87,12 @@ ojo_create_project <- function(name = NULL, dir = ".", private = TRUE) {
 
   # Commit changes
   gert::git_commit(
-    message = "Scaffold file structure",
+    message = "Scaffold project",
     repo = project_dir
   )
 
   # Push changes to Github
   gert::git_push(repo = project_dir)
-
-  # Change working directory back
-  setwd(init_wd)
-  usethis::proj_set(init_wd)
 
   # Return the directory of the project invisibly
   invisible(project_dir)
